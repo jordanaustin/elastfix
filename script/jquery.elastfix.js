1b319e900d9be088536e34ab59b1557dd609f54a
Jordan <jordan@missingdesign.com>
2012-05-01 12:53:39 -0700

    Added MIT licensing info

--- script/jquery.elastfix.js
+++ script/jquery.elastfix.js
@@ -1,3 +1,13 @@
+/*
+ * ElastFix v0.1
+ * Copyright 2012 Jordan Austin
+ * http://missingdesign.com
+ *
+ * Released under MIT license, 
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+ 
+ 
 (function($){
 	$.fn.elastfix = function(options) {
 		var

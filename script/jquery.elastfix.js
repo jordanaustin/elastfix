5542d58a6e16610567cf884cd939509cb86d719f
Jordan <jordan@missingdesign.com>
2012-05-01 13:01:11 -0700

    Condensed some of the code, to make it simpler and smaller yet.

--- script/jquery.elastfix.js
+++ script/jquery.elastfix.js
@@ -7,39 +7,29 @@
  * http://www.opensource.org/licenses/mit-license.php
  */
  
- 
 (function($){
 	$.fn.elastfix = function(options) {
 		var
 			defaults = {
-				autoResize: true // Auto checks if the scroll content size has changed on touchstart. 
+				autoResize: true // Auto checks if the scroll content size has changed on touchstart. Recommended. 
 			},
 
 		settings = $.extend({}, defaults, options);
          
 		this.each(function() {
 			var $this = $(this);
-				negHeight = - ($('> *', this).height() - $this.height()),
-    			posHeight = $('> *', this).height() - $this.height() - 1;
-
+			    negHeight = - ($('> *', this).height() - $this.height()),
+    			    posHeight = $('> *', this).height() - $this.height() - 1;
+			    
+			// Only stops this event from propatating when the scrolling element has a height greater than the parent.    
 			$this.on('touchmove', function(e){
-				if($('> *', this).position().top === 0) {
-					$(this).scrollTop(1);
-				}
-				else if($('> *', this).position().top === negHeight) {
-					$(this).scrollTop(posHeight);
-				}
-
 				if($('> *', this).height() > $this.height()) {
 					e.stopPropagation();
-				}
+				}	
 			});
-			
-			$this.on('touchstart', function(e){
-				if (settings.autoResize === true) {
-					var negHeight = - ($('> *', this).height() - $this.height()),
-    					posHeight = $('> *', this).height() - $this.height() - 1;
-    			}
+			$this.on('touchstart touchmove', function(e) {
+				    var negHeight = - ($('> *', this).height() - $this.height()),
+					posHeight = $('> *', this).height() - $this.height() - 1;
 				if($('> *', this).position().top === 0) {
 					$(this).scrollTop(1);
 				}
